3b9695d8ddde7157eb9018340ae2dfa89de60094
Joe Koberg <joe.koberg@versionone.com>
2013-06-03 09:17:59 -0400

    Add command line flags for parameters

--- oauth2proxy/proxy.py
+++ oauth2proxy/proxy.py
@@ -1,5 +1,6 @@
 
 import json, random, logging, os, sys, urlparse
+from pprint import pprint as pp, pformat as pf
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -11,15 +12,36 @@ import urllib3
     
 # Bring in Google oauth2 library functions
 
+import gflags
+
 import oauth2client
 import oauth2client.clientsecrets
 import oauth2client.tools
 from oauth2client.file import Storage
 from oauth2client.client import flow_from_clientsecrets 
 
+gflags.DEFINE_string('destination', None, 'HTTP server to forward requests to')
+gflags.DEFINE_string('scope', 'query-api-1.0', 'oauth2 scope to request')
+gflags.DEFINE_string('secrets', 'client_secrets.json', 'File to read oauth2 client id and client secret from')
+gflags.DEFINE_string('creds', 'stored_credentials.json', 'File to store oauth2 client tokens in')
+gflags.DEFINE_string('localuser', 'local_userpass.json', 'File to store randomly generated "basic" auth creds')
+gflags.DEFINE_boolean('prompt', True, 'prompt user to grant credentials')
+gflags.DEFINE_boolean('anycert', True, 'accept any SSL cert (even unknown ones)')
+
+gflags.DEFINE_string('listenaddr', '127.0.0.1', 'Address to  listen on')
+gflags.DEFINE_integer('listenport', 5180, 'Port for inbound requests')
+
+FLAGS = gflags.FLAGS
+try:
+    argv = FLAGS(sys.argv)  # parse flags
+except gflags.FlagsError, e:
+    print '%s\\nUsage: %s ARGS\\n%s' % (e, sys.argv[0], FLAGS)
+    sys.exit(1)
+
+
 
 class JsonUserPasswordFile:
-    def __init__(self, filename="local_user_creds.json"):
+    def __init__(self, filename=FLAGS.localuser):
         self.filename = filename
         self.get_user_details()
 
@@ -54,10 +76,10 @@ class JsonUserPasswordFile:
 class DealWithUnauthorizedError(Exception): pass
 
 class OAuth2Proxy(object):
-    def __init__(self, scopes, client_secrets_file='client_secrets.json', oauth2_creds_file='stored_credentials.json', run_grant=True):
+    def __init__(self, scopes, client_secrets_file=FLAGS.secrets, oauth2_creds_file=FLAGS.creds, run_grant=FLAGS.prompt):
         self.client_secrets_file = client_secrets_file
         self.oauth2_creds_file = oauth2_creds_file
-        self.oauth_client = httplib2.Http(disable_ssl_certificate_validation=True)
+        self.oauth_client = httplib2.Http(disable_ssl_certificate_validation=FLAGS.anycert)
         self.open_clientsecrets(scopes)
         self.credentials = self.storage.get()
         if not self.credentials:
@@ -67,9 +89,13 @@ class OAuth2Proxy(object):
                 logging.error("Please obtain the Oauth2 permission grant from the user by re-running this tool with option TODO.")
                 raise Exception("Missing oauth2 credentials in %s"%(self.oauth2_creds_file,))
 
-        parts = urlparse.urlparse(self.secrets_data["server_base_uri"])
-        scheme, netloc, path, params, query, fragment = parts
-        self.proxy_destination = urlparse.urlunparse((scheme, netloc, "", params, query, fragment))
+        if FLAGS.destination:
+            self.proxy_destination = FLAGS.destination
+        else:
+            # take path part off, because requests will include it
+            parts = urlparse.urlparse(self.secrets_data["server_base_uri"])
+            scheme, netloc, path, params, query, fragment = parts
+            self.proxy_destination = urlparse.urlunparse((scheme, netloc, "", params, query, fragment)) 
         logging.info("Proxying to " + self.proxy_destination)
         self.proxy = WSGIProxyApp(self.proxy_destination)
 
@@ -95,7 +121,7 @@ class OAuth2Proxy(object):
         self.credentials.apply(headers)
         for header, content in headers.items():
             environ["HTTP_"+header.upper()] = content
-        logging.debug(environ)
+        logging.debug(pf(environ))
         return environ
 
     def __call__(self, environ, upstream_start_response):
@@ -103,6 +129,8 @@ class OAuth2Proxy(object):
         # request to the downstream (versionone) server and has a
         # response header from it.
         def my_start_response(statusline, headerlist):
+            logging.debug(statusline)
+            logging.debug(pf(headerlist))
             if statusline.startswith('401'):
                 raise DealWithUnauthorizedError((statusline,headerlist))
             return upstream_start_response(statusline, headerlist)
@@ -117,18 +145,13 @@ class OAuth2Proxy(object):
             return self.proxy(environ, upstream_start_response)
 
 
-if __name__ == '__main__':
-    # Handle command-line args for google oauth2 library
-
-    import gflags
-    FLAGS = gflags.FLAGS
-    try:
-        argv = FLAGS(sys.argv)  # parse flags
-    except gflags.FlagsError, e:
-        print '%s\\nUsage: %s ARGS\\n%s' % (e, sys.argv[0], FLAGS)
-        sys.exit(1)
-
+def main():
     passfile = JsonUserPasswordFile()
-    proxy_app = OAuth2Proxy(scopes="query-api-1.0")
+    proxy_app = OAuth2Proxy(scopes=FLAGS.scope)
     authed_app = AuthBasicHandler(proxy_app, "oauth2proxy", passfile.check_user_pass)
-    httpserver.serve(authed_app, host='127.0.0.1', port='5180')
+    httpserver.serve(authed_app, host=FLAGS.listenaddr, port=FLAGS.listenport)
+
+
+if __name__ == '__main__':
+    main()
+
